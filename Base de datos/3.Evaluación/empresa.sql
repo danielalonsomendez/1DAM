afa20c09f372a3650a5bab3ca5d12d77f34c7255
danielalonsomendez <danielalonso308@gmail.com>
2025-03-25 17:51:58 +0100

    bbdd tema 11

--- Base de datos/3.Evaluación/empresa.sql
+++ Base de datos/3.Evaluación/empresa.sql
@@ -113,3 +113,155 @@ update VistaEmple set dept_no=20 where apellido="TOVAR";
 /*d)Elimine, si es posible, al empleado apellidado TOVAR a través de la vista
 VistaEmple.*/
 delete from VistaEmple where apellido="TOVAR";
+
+
+/* TEMA 11 */
+/*
+3. Escriba una función llamada NumSubordinados que reciba el número de un empleado de la tabla
+Emple y que devuelva el número de subordinados que tiene ese empleado, o lo que es lo mismo,
+el número de empleados de los cuales es director o jefe directo. Recuerde que el atributo dir de
+la tabla Emple indica el número del empleado director o jefe directo. Escriba un ejemplo de
+llamada a la función.
+*/
+delimiter //
+create function NumSubordinados(empno int) returns int DETERMINISTIC
+begin 
+return (select count(dir) from emple where dir = empno);
+end;
+//
+select NumSubordinados(7839);
+//
+/*
+4. Cree una función llamada SalarioJefe que reciba el apellido de un empleado y que devuelva un
+número real que tome el valor del salario del empleado dividido entre el número de empleados
+subordinados que tenga. En caso de que el empleado no tenga subordinados, la función deberá
+devolver el valor -1.
+*/
+delimiter //
+create function SalarioJefe(ApellidoEmpleado varchar(40)) returns int DETERMINISTIC
+begin 
+declare subordinados int;
+declare empno int;
+declare salarioEmp float;
+select emp_no,salario into empno,salarioEmp from emple where apellido = ApellidoEmpleado;
+select NumSubordinados(empno) into subordinados ;
+if subordinados=0 then
+return -1;
+else 
+return salarioEmp/subordinados;
+end if;
+end;
+//
+select SalarioJefe("REY");
+//
+/*
+5. Cree un procedimiento llamado AñadirDepar que reciba el nombre de un departamento y su
+localidad. Se debe insertar ese departamento en la tabla Depart asignándole como número el que
+resulte de sumar 10 al número más alto de los departamentos de la empresa. Al final muestre un
+mensaje con el texto: “Se ha creado un nuevo departamento con el no xxx”.
+*/
+delimiter //
+create procedure AñadirDepar(NombreDepartamento varchar(40),Localidad varchar(40)) 
+begin 
+declare maxDepart int;
+declare id int;
+select max(dept_no) into maxDepart from depart;
+select (10+maxDepart) into id;
+insert into depart values((10+maxDepart),NombreDepartamento,Localidad);
+select concat("Se ha creado un nuevo departamento con el no ",id)"Mensaje";
+end;
+//
+call AñadirDepar("TEST3","BARCELONA");
+//
+/*
+6. Cree un procedimiento llamado ModificarSalario que reciba el número de un empleado y un número
+entero. El procedimiento debe modificar el salario del empleado con el no recibido como primer
+parámetro según el porcentaje pasado como segundo parámetro y mostrar el salario del empleado
+antes y después de la modificación.
+*/
+delimiter //
+create procedure ModificarSalario(empno int,num int) 
+begin 
+declare salarioAntes int;
+declare salarioDespues int;
+select salario into salarioAntes from emple where emp_no = empno;
+select (salarioAntes + ((salarioAntes *num)/100)) into salarioDespues;
+update emple set salario = salarioDespues where emp_no=empno;
+select concat("Salario de ",salarioAntes,"€ a ",salarioDespues,"€")"Mensaje";
+end;
+//
+call ModificarSalario(7839,50);
+//
+/*
+7. Cree un procedimiento llamado AsignarComision que reciba un número de empleado y
+compruebe su salario en la tabla Emple de la base de datos Empresa. Si el salario del empleado
+es menor que 1000 €, se le debe asignar una comisión que será el 5% del salario; en caso de que
+su salario sea superior o igual a 1000 €, pero inferior a 2000 €, se le deberá asignar una
+comisión igual al 2,5% del salario; en caso de que cobre 2000 € o más, se le pondrá a 0 € la
+comisión. Muestre un mensaje como el siguiente: “Al empleado no XXXX se le ha asignado
+una comisión de YY.YY €”.
+*/
+delimiter //
+create procedure AsignarComision(empno int) 
+begin 
+declare salarioEmpleado int;
+declare comisionEmpleado double;
+declare comisionDinero double;
+select salario into salarioEmpleado from emple where emp_no = empno;
+if salarioEmpleado<1000 then
+set comisionEmpleado = 5;
+elseif salarioEmpleado>1000 and salarioEmpleado<2000 then
+set comisionEmpleado = 2.5;
+elseif salarioEmpleado>2000 then
+set comisionEmpleado = 0;
+end if;
+select ((salarioEmpleado*comisionEmpleado)/100) into comisionDinero;
+update emple set comision= comisionDinero where emp_no = empno; 
+select concat("Al empleado no ",empno," se le ha asignado una comisión de ",comisionDinero,"€")"Mensaje";
+end;
+//
+call AsignarComision(7839);
+//
+/*
+8. Cree una función llamada PorcenSalarial que reciba el número de un empleado y nos devuelva
+un número real que indique el porcentaje que supone el salario del empleado en relación con la
+suma salarial de todos los empleados de su departamento.
+*/
+delimiter //
+create function PorcenSalarial(empno int) returns int DETERMINISTIC
+begin 
+declare sumaSalarial double;
+declare salarioEmp float;
+select salario into salarioEmp from emple where emp_no = empno;
+select sum(salario) into sumaSalarial from emple E join depart D on E.dept_no=D.dept_no where E.dept_no= (select dept_no from emple where emp_no=empno);
+return salarioEmp/sumaSalarial*100;
+end;
+//
+select PorcenSalarial(7654);
+//
+
+/*
+9. Cree un procedimiento llamado ModificarSalariosDepar que reciba el número de un
+departamento y un número real. El procedimiento, en primer lugar, obtendrá el número de
+empleados que trabajan en el departamento cuyo número se ha recibido como primer parámetro.
+En caso de que no trabaje ningún empleado en dicho departamento, se mostrará el mensaje “En
+el dpto. no xxx no trabaja ningún empleado”. En caso contrario, se deberá modificar el salario de
+los empleados del departamento cuyo número se ha pasado como primer parámetro en base al
+porcentaje recibido como segundo parámetro, después de lo cual se mostrará el mensaje “Se ha
+modificado el salario de todos los empleados del dpto. no xx en un yyy,yy%”.
+*/
+delimiter //
+create procedure ModificarSalariosDepar(deptno int,num double) 
+begin 
+declare numEmpleados int;
+select max(dept_no) into numEmpleados from depart where dept_no = deptno;
+if numEmpleados=0 then
+select concat("En el dpto. no ",deptno," no trabaja ningún empleado ")"Mensaje";
+else 
+update emple set salario= salario+((salario*num)/100) where dept_no=deptno;
+select concat("Se ha modificado el salario de todos los empleados del dpto. no  ",deptno," en un ",num,"%")"Mensaje";
+end if;
+end;
+//
+call ModificarSalariosDepar(60,30);
+//
